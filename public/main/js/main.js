3053e58d3d9669aa67c2370003b7b772d840b6f8
raphael zimmer <guidozimmer@mindstream.de>
2024-11-21 17:43:35 +0100

    update number animation

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -14,15 +14,52 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
 anime({
     targets: { value: 0 }, // Start value
-    value: 1000, // End value
+    value: 1142, // End value
     duration: 2000, // Duration in milliseconds
     easing: 'easeOutQuad', // Easing function
     round: 1, // Round to the nearest whole number
-    update: function(anim) {
-        // Update the number displayed
-        document.getElementById('animated-number').textContent = anim.animatables[0].target.value;
+    update: function (anim) {
+        // Format the number with thousand separators
+        const formattedNumber = anim.animatables[0].target.value.toLocaleString('de-DE');
+        document.getElementById('animatedNumber1').textContent = formattedNumber + " Projekte";
+    }
+});
+
+anime({
+    targets: { value: 0 }, // Start value
+    value: 48249, // End value
+    duration: 2000, // Duration in milliseconds
+    easing: 'easeOutQuad', // Easing function
+    round: 1, // Round to the nearest whole number
+    update: function (anim) {
+        const formattedNumber = anim.animatables[0].target.value.toLocaleString('de-DE');
+        document.getElementById('animatedNumber2').textContent = formattedNumber + " ha";
+    }
+});
+
+anime({
+    targets: { value: 0 }, // Start value
+    value: 48, // End value
+    duration: 2000, // Duration in milliseconds
+    easing: 'easeOutQuad', // Easing function
+    round: 1, // Round to the nearest whole number
+    update: function (anim) {
+        const formattedNumber = anim.animatables[0].target.value.toLocaleString('de-DE');
+        document.getElementById('animatedNumber3').innerHTML = formattedNumber + " GW<sub>p</sub>";
+    }
+});
+
+
+anime({
+    targets: { value: 0 }, // Start value
+    value: 96, // End value
+    duration: 2000, // Duration in milliseconds
+    easing: 'easeOutQuad', // Easing function
+    round: 1, // Round to the nearest whole number
+    update: function (anim) {
+        const formattedNumber = anim.animatables[0].target.value.toLocaleString('de-DE');
+        document.getElementById('animatedNumber4').textContent = formattedNumber + " GW Batterie";
     }
 });
