78ec5ea605e49d9bd722bb46ef237a7e038f1847
raphael zimmer <guidozimmer@mindstream.de>
2024-11-24 17:48:22 +0100

    trst

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -18,11 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
 const values = [
 
     
-    [1142, "Solarprojekte", "935px", "1000px"], // First slide: number at 50px, text at 75px
-    [48249, "ha Flächen", "950px", "1000px"], // Second slide: number at 100px, text at 12px
-    [48, "GW<sub>p</sub> Leistung", "1000px", "1000px"], // Third slide: number at 20px, text at 40px
-    [96, "GW Batterieleistung", "1000px", "800px"], // Fourth slide: number at 10px, text at 90px
-    [0, "Machen wir was draus!", "-5000px", "900px"] // Final slide: default positions
+    [1142, "Solarprojekte", "-935px", "-1000px"], // First slide: number at 50px, text at 75px
+    [48249, "ha Flächen", "-950px", "-1000px"], // Second slide: number at 100px, text at 12px
+    [48, "GW<sub>p</sub> Leistung", "-1000px", "-1000px"], // Third slide: number at 20px, text at 40px
+    [96, "GW Batterieleistung", "-1000px", "-800px"], // Fourth slide: number at 10px, text at 90px
+    [0, "Machen wir was draus!", "-5000px", "-900px"] // Final slide: default positions
 ];
 
 // Start the fade effect
