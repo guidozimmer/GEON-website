6cc154f44ee0340dfc81281e1822f979de91a9c6
raphael zimmer <guidozimmer@mindstream.de>
2024-11-24 17:52:40 +0100

    test

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -7,6 +7,7 @@ import { startNumberFade } from "./numberFade.js"
 loadTextFile('websiteText.txt', injectContent);
 //barGraphs();
 
+console.log("test");
 
 document.addEventListener('DOMContentLoaded', function () {
     if (typeof setupDropdown === 'function') {
@@ -15,9 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-const values = [
-
-    
+const values = [    
     [1142, "Solarprojekte", "0", "500px"], // First slide: number at 50px, text at 75px
     [48249, "ha Flächen", "950px", "1000px"], // Second slide: number at 100px, text at 12px
     [48, "GW<sub>p</sub> Leistung", "1000px", "1000px"], // Third slide: number at 20px, text at 40px
@@ -27,3 +26,5 @@ const values = [
 
 // Start the fade effect
 startNumberFade("number", "text", "numberDisplay", values, 4000); // 4-second interval
+
+console.log("test1");
