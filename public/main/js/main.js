1f37458efdd072f4c30cdb671095f79f1d7a2615
raphael zimmer <guido.zimmer@mindstream.de>
2024-11-25 16:07:14 +0100

    update background video

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // Values to cycle through
-const values = ["1.142 Solarprojekte", "48.249 ha Flächen", "48 GW<sub>p</sub> Leistung", "96 GW Batterieleistung", "Machen wir was draus!"];
+const values = ["1.142 Solarprojekte", "48.249 ha Flächen", "48 GW<sub>p</sub> Leistung", "96 GW Batterieleistung", "Machen wir etwas daraus!"];
 
 // Start the fader
 startNumberFade("numberDisplay", values);
