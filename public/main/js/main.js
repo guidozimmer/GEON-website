41d2d7b7ae1771d4433e94ccd5c799a223a5660b
Tristan Zimmer <tristan@Gustavo-Fring-LAN.fritz.box>
2025-01-18 16:28:34 +0100

    test

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -2,7 +2,6 @@ import { setupDropdown } from "./nav.js";
 import { startNumberFade } from "./numberFade.js";
 import { createLanguageSwitcher } from './websiteText.js';
 import { redirectToCode } from './redirectForm.js';
-import {initializeCookieConsent} from './cookieconsent-init.js';
 
 const languageSwitcher = createLanguageSwitcher();
 
