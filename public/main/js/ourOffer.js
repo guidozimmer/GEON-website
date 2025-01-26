0bf751a6aad454c2b7b6d327fdcb22985deb2103
Tristan Zimmer <tristan@Gustavo-Fring-2.fritz.box>
2025-01-26 13:27:41 +0100

    update ourOffer

--- public/main/js/ourOffer.js
+++ public/main/js/ourOffer.js
@@ -30,10 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Function to hide all forms
     function hideAllForms() {
-        landOwnerForm.style.display = 'none';
-        investorForm.style.display = 'none';
-        communityForm.style.display = 'none';
-        
         landOwnerSquare.classList.remove('active');
         investorSquare.classList.remove('active');
         communitySquare.classList.remove('active');
